6c7b4206f485a8b7854f86785de7f082fdb79875
Van-Thai Nguyen <van-thai.nguyen@ant-tech.eu>
2018-08-07 10:17:20 +0700

    update noke device object structure sample

--- ios/RNNoke.swift
+++ ios/RNNoke.swift
@@ -107,11 +107,11 @@ class RNNoke : RCTEventEmitter, NokeDeviceManagerDelegate {
         rejecter reject: RCTPromiseRejectBlock
         ) {
         /**
-         * name: "New Lock"
-         * mac: "CB:BC:87:3B:CB:D7"
-         * key: "9966eb079eabb129e7adbded88eab6c3
-         * cmd: "0152843f00a2dec3515b000000000000000000ef"
-         */
+        * name: "Lock Name"
+        * mac: "XX:XX:XX:XX:XX:XX"
+        * key: "OFFLINE_KEY"
+        * cmd: "OFFLINE_COMMAND"
+        */
         let noke = NokeDevice.init(
             name: data["name"]! as String,
             mac: data["mac"]! as String
