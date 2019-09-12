e8e16c7c5d8185589c5de552d4bcc30c78a9923c
linh <tuan-linh.tran@ant-tech.eu>
2019-09-12 10:29:30 +0700

    Fix deprecated API

--- ios/Noke.swift
+++ ios/Noke.swift
@@ -8,14 +8,6 @@
 import Foundation
 import NokeMobileLibrary
 
-class NokeError {
-    static var NO_LOCK_CONNECTED = "100"
-    static var NO_LOCK_SESSION = "101"
-    
-    static var NO_LOCK_CONNECTED_MESSAGE = "No lock connected"
-    static var NO_LOCK_SESSION_MESSAGE = "No lock session"
-}
-
 @objc(Noke)
 class Noke : RCTEventEmitter, NokeDeviceManagerDelegate {
     var currentNoke: NokeDevice?
@@ -49,7 +41,7 @@ class Noke : RCTEventEmitter, NokeDeviceManagerDelegate {
             isConnected = false
             NokeDeviceManager.shared().startScanForNokeDevices()
             sendEvent(withName: "disconnected", body: nokeDeviceInfoFactory(noke))
-            removeAllLock()
+            removeAll()
             break
         }
     }
